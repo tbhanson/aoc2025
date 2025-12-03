5fcb9b176cbb17e58283d31ca21e024a8892ebe1
Tim Hanson <tbh391@gmail.com>
2025-12-03 11:20:26 +0100

    solved day 1, part 2

--- day1.rkt
+++ day1.rkt
@@ -17,7 +17,9 @@
   ;
   [read-lock-rotations (-> port? stream?)]
   [next-position (-> exact-nonnegative-integer? lock-rotation? exact-nonnegative-integer?)]
-  [get-password-from-lock-rotations (-> stream? exact-nonnegative-integer?)]
+  [get-password-from-lock-rotations-part1 (-> stream? exact-nonnegative-integer?)]
+  [next-position-and-zero-count (-> exact-nonnegative-integer? lock-rotation? (values exact-nonnegative-integer? exact-nonnegative-integer? ))]
+  [get-password-from-lock-rotations-part2 (-> stream? exact-nonnegative-integer?)]
   )
  )
 
@@ -45,6 +47,7 @@
 (define (read-lock-rotations a-port)
    (lock-rotation-lexer a-port))
 
+; in some sense we solved the first part with some optimization
 (define (next-position current-position next-lock-rotation)
   (let ([how-far-mod-100 (remainder (lock-rotation-how-far next-lock-rotation) 100)])
     (let ([add-to-position
@@ -56,9 +59,41 @@
         (if (< almost-next-position 0)
             (+ 100 almost-next-position)
             (remainder almost-next-position 100))))))
-             
 
-(define (get-password-from-lock-rotations stream-of-lock-rotations)
+; it might make sense to solve the second part more naively: tick positions one at a time and count zeroes as we go
+(define (next-position-and-zero-count current-position next-lock-rotation)
+  (define (iter pos-so-far zero-count-so-far rotation-remaining)
+    (cond [(= rotation-remaining 0)
+           (values pos-so-far zero-count-so-far)]
+          [(< rotation-remaining 0) ; click one to the left
+           (let ([new-pos-almost (- pos-so-far 1)])
+             (let ([new-pos
+                    (if (< new-pos-almost 0)
+                        (+ 100 new-pos-almost)
+                        new-pos-almost)])
+               (let ([zero-count-increment
+                      (if (= new-pos 0)
+                          1
+                          0)])
+                 (iter new-pos (+ zero-count-so-far zero-count-increment) (+ rotation-remaining 1)))))]
+          [else ; click one to the right
+           (let ([new-pos (remainder (+ pos-so-far 1) 100)])
+             (let ([zero-count-increment
+                    (if (= new-pos 0)
+                        1
+                        0)])
+               (iter new-pos (+ zero-count-so-far zero-count-increment) (- rotation-remaining 1))))]))
+
+  (if (equal? (lock-rotation-direction next-lock-rotation) #\R)
+      (iter current-position 0 (lock-rotation-how-far next-lock-rotation))       ; right is positive
+      (iter current-position 0 (- 0 (lock-rotation-how-far next-lock-rotation))) ; left is negative
+      ))
+
+                  
+              
+
+
+(define (get-password-from-lock-rotations-part1 stream-of-lock-rotations)
   (let-values ([(answer final-pos)
                 (for/fold ([result 0]  ; start zero count at 0
                            [current-pos 50] ; start position at 50
@@ -69,5 +104,16 @@
                         (values (+ result 1) new-pos)
                         (values result new-pos))))])
     answer))
-          
+
+(define (get-password-from-lock-rotations-part2 stream-of-lock-rotations)
+  (let-values ([(answer final-pos)
+                (for/fold ([result 0]  ; start zero count at 0
+                           [current-pos 50] ; start position at 50
+                           )
+                          ([next-lock-rotation stream-of-lock-rotations])
+                  (let-values ([(new-pos zero-count-inc)
+                                (next-position-and-zero-count current-pos next-lock-rotation)])
+                    (values (+ result zero-count-inc) new-pos)))])
+    answer))
+
       
--- test-day1.rkt
+++ test-day1.rkt
@@ -18,6 +18,7 @@
        (lock-rotation #\L 32)
        ))
 
+; read-lock-rotations
 (check-true
  (stream?
   (read-lock-rotations (open-input-file "test-data/input-day1-1.txt"))))
@@ -27,6 +28,7 @@
   (read-lock-rotations (open-input-file "test-data/input-day1-1.txt")))
  4531)
 
+; next-position
 (check-equal?
  (next-position 0 (lock-rotation #\R 5))
  5)
@@ -42,13 +44,43 @@
 ; get-password-from-lock-rotations -- test on their small sample input 
 
 (check-equal?
- (get-password-from-lock-rotations
+ (get-password-from-lock-rotations-part1
   (read-lock-rotations (open-input-string "L68 L30 R48 L5 R60 L55 L1 L99 R14 L82 ")))
  3)
 
 ;; (check-equal?
-;;  (get-password-from-lock-rotations
+;;  (get-password-from-lock-rotations-part1
 ;;   (read-lock-rotations (open-input-file "test-data/input-day1-1.txt")))
 ;;  what me be a spoiler? )
 
+; (next-position-and-zero-count current-position next-lock-rotation)
+
+(check-true
+ (let-values ([(new-pos zero-hits)
+               (next-position-and-zero-count 0 (lock-rotation #\R 5))])
+   (and (= new-pos 5)
+        (= zero-hits 0))))
+
+(check-true
+ (let-values ([(new-pos zero-hits)
+               (next-position-and-zero-count 0 (lock-rotation #\L 5))])
+   (and (= new-pos 95)
+        (= zero-hits 0))))
+
+(check-true
+ (let-values ([(new-pos zero-hits)
+               (next-position-and-zero-count 1 (lock-rotation #\L 3))])
+   (and (= new-pos 98)
+        (= zero-hits 1))))
+
+; get-password-from-lock-rotations-part2
+(check-equal?
+ (get-password-from-lock-rotations-part2
+  (read-lock-rotations (open-input-string "L68 L30 R48 L5 R60 L55 L1 L99 R14 L82 ")))
+ 6)
+
+;; (check-equal?
+;;  (get-password-from-lock-rotations-part2
+;;   (read-lock-rotations (open-input-file "test-data/input-day1-1.txt")))
+;;  what me be a spoiler? )
 
