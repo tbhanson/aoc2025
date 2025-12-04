aba043d95fbc5e977963cc602066416484a7d8b4
Tim Hanson <tbh391@gmail.com>
2025-12-04 11:31:07 +0100

    day 2 part 2 presumed solved

--- day2.rkt
+++ day2.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require parser-tools/lex)
+(require math/number-theory)
 
 
 (provide
@@ -11,6 +12,10 @@
   [invalid-ID (-> exact-nonnegative-integer? boolean?)]
   [invalid-IDs-in-range (-> exact-nonnegative-integer? exact-nonnegative-integer? stream?)]
   [sum-invalid-IDs-in-ID-range-stream (-> stream? exact-nonnegative-integer?)]
+  ; part 2
+  [string-is-N-copies? (-> string? exact-nonnegative-integer? boolean?)]
+  [invalid-ID-2 (-> exact-nonnegative-integer? boolean?)]
+  [sum-invalid-IDs-2-in-ID-range-stream (-> stream? exact-nonnegative-integer?)]
   )
  )
 
@@ -82,4 +87,61 @@
              (car next-range)
              (cdr next-range)))])
       (+ result next-partial-sum))))
+
+
+(define (string-is-N-copies? a-string n)
+  ;(printf "(string-is-N-copies? ~a ~a)~n" a-string n)
+  (let ([sub-str-len (/ (string-length a-string) n)])
+    ;(printf " sub-str-len: ~a~n" sub-str-len)
     
+    (let ([prefix (substring a-string 0 sub-str-len)])
+      ;(printf " prefix: ~a~n" prefix)
+      
+      (for/fold ([result #t])
+                ([i (in-range n)])
+        ;(printf " i: ~a~n" i)
+        (let ([next-result
+               (and result
+                    (string=?
+                     prefix
+                     (substring
+                      a-string
+                      (* i sub-str-len)
+                      (* (+ i 1) sub-str-len))))])
+          ;(printf " next-result: ~a~n" next-result)
+          next-result)))))
+       
+; now we have to consider any number of copies 2 or more
+(define (invalid-ID-2 id)
+  (let ([id-as-str (number->string id)])
+    (let ([id-str-len (string-length id-as-str)])
+      (define (iter remaining-divisors)
+        (cond [(null? remaining-divisors)
+               #f]
+
+              [(string-is-N-copies? id-as-str (car remaining-divisors))
+               #t]
+
+              [else
+               (iter (cdr remaining-divisors))]))
+
+      (let ([try-divisors
+             (cdr (divisors id-str-len))]) ; we need to try all divisors except 1
+        (iter try-divisors)))))
+
+(define (invalid-IDs-2-in-range lo hi)
+  (stream-filter
+   invalid-ID-2
+   (in-range lo (+ hi 1))
+   ))
+
+(define (sum-invalid-IDs-2-in-ID-range-stream ID-range-stream)
+  (for/fold ([result 0])
+            ([next-range ID-range-stream])
+    (let ([next-partial-sum
+           (stream-fold
+            + 0
+            (invalid-IDs-2-in-range
+             (car next-range)
+             (cdr next-range)))])
+      (+ result next-partial-sum))))
--- test-day2.rkt
+++ test-day2.rkt
@@ -73,3 +73,24 @@
 ;;  17)
 
 
+; invalid-ID-2
+(check-false
+ (invalid-ID-2 12))
+ 
+(check-true
+ (invalid-ID-2 11))
+
+(check-true
+ (invalid-ID-2 111))
+
+; check their small test input
+(check-equal?
+ (sum-invalid-IDs-2-in-ID-range-stream
+  (read-ID-ranges
+   (open-input-string 
+    (string-append
+     "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,"
+     "1698522-1698528,446443-446449,38593856-38593862,565653-565659,"
+     "824824821-824824827,2121212118-2121212124"))))
+ 4174379265)
+
