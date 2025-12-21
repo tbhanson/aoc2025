d03addff2a780d6273950cf81b9560caed58dd18
Tim Hanson <tbh391@gmail.com>
2025-12-21 21:09:55 +0100

    day 6 begin part 2

--- day6.rkt
+++ day6.rkt
@@ -1,12 +1,18 @@
 #lang racket
 
 (require parser-tools/lex)
+(require math/array)
 
 
 (provide
  (contract-out
   [read-cephalapod-arithmetic (-> port? stream?)]
   [solve-cephalapod-arithmetic (-> stream? exact-nonnegative-integer?)]
+
+  [read-lines (-> port? stream?)] ; stream of lines top to bottom as expected
+  [vector-vector-transpose (-> vector? vector?)]
+  [read-cephalapod-arithmetic-part-2 (-> port? vector?)]
+  [solve-cephalapod-arithmetic-part-2 (-> stream? exact-nonnegative-integer?)]
   )
  )
 
@@ -70,5 +76,46 @@
       (for/fold ([sum 0])
                 ([column-result column-answers])
         (+ sum column-result)))))
-                            
+
+
+; and now for something completely different
+
+(define (read-lines a-port)
+  (let ([next-line (read-line a-port)])
+    (if (eof-object? next-line)
+        empty-stream
+        (stream-cons next-line (read-lines a-port)))))
+
+(define (vector-vector-transpose v-v)
+  (let ([row-count (vector-length v-v)]
+        [col-count (vector-length (vector-ref v-v 0))])
+    (list->vector
+     (reverse
+      (for/fold ([new-rows-list '()])
+                ([new-row-num (in-range col-count)])
+        (let ([next-row
+               (for/list ([old-row (in-range row-count)])
+                 (vector-ref (vector-ref v-v old-row) new-row-num))])
+          (cons
+           (list->vector next-row)
+           new-rows-list)))))))
+
+(define (read-cephalapod-arithmetic-part-2 a-port)
+  (let ([vector-of-line-vectors
+         (list->vector
+          (stream->list
+           (stream-map
+            (lambda (line)
+              (printf "line: ~a~n" line)
+              (list->vector
+               (string->list line)))
+            (read-lines a-port))))])
+    (printf "vector-of-line-vectors: ~a~n" vector-of-line-vectors)
+    (vector-vector-transpose
+     vector-of-line-vectors)))
+     
+   
+
+(define (solve-cephalapod-arithmetic-part-2 line-stream)
+  0)
          
--- test-day6.rkt
+++ test-day6.rkt
@@ -2,7 +2,7 @@
 
 (require rackunit "day6.rkt")
 
-; read input
+; part 1
 (let ([in-port
        (open-input-string
         (format
@@ -34,3 +34,42 @@
 ;;      (solve-cephalapod-arithmetic ceph-arith)
 ;;      ...
 ;;      )))
+
+; part 2
+(check-equal?
+ (vector-vector-transpose
+  #(#(1 2 3) #(4 5 6)))
+ #(#(1 4) #(2 5) #(3 6)))
+
+;; (let ([input-string
+;;        (format
+;;         (string-append
+;;          "123 328  51 64 ~n"
+;;          " 45 64  387 23 ~n"
+;;          "  6 98  215 314~n"
+;;          "*   +   *   +  ~n"))])
+;; 
+;;   (let ([in-port
+;;          (open-input-string input-string)])
+;;     (let ([line-stream (read-lines in-port)])
+;;       (check-equal?
+;;        (stream-length line-stream)
+;;        4)
+;;       (check-equal?
+;;        (stream-first line-stream)
+;;        "123 328  51 64 ")))
+;; 
+;;   (let ([in-port
+;;          (open-input-string input-string)])
+;; 
+;;     (check-equal?
+;;      (read-cephalapod-arithmetic-part-2 in-port)
+;;      "*  1")
+;; ;;      #(#\space #\4 #\space #\space))
+;; ;;     (check-equal?
+;; ;;      (solve-cephalapod-arithmetic-part-2 ceph-arith)
+;; ;;      3263827
+;;      
+;;      
+;;   
+;;     ))
