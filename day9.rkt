30246b9d4d88d1fc0475bfa021c88c78322fe3e9
Tim Hanson <tbh391@gmail.com>
2026-01-07 12:15:25 +0100

    day 9 part 1: solve their sample

--- day9.rkt
+++ day9.rkt
@@ -18,7 +18,22 @@
                  (cons
                   (car next-tuple)
                   (cadr next-tuple))])
-          (stream-cons next-pair (read-positions in-port)))))))
+            (stream-cons next-pair (read-positions in-port)))))))
 
 (define (max-rectangle in-port)
-  0)
+  (let ([all-corners (read-positions in-port)])
+    (for*/fold ([max-rectangle-size 0])
+               ([c1 all-corners]
+                [c2 all-corners]
+                #:unless (equal? c1 c2))
+      ;(printf "c1: ~a; c2: ~a~n" c1 c2)
+      (let ([x-diff (+ 1 (abs (- (car c1) (car c2))))]
+            [y-diff (+ 1 (abs (- (cdr c1) (cdr c2))))])
+        ;(printf "x-diff: ~a; y-diff: ~a~n" x-diff y-diff)
+        (if (> (* x-diff y-diff) max-rectangle-size)
+            (* x-diff y-diff)
+            max-rectangle-size)))))
+          
+              
+    
+  
