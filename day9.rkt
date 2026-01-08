6c2ef518dbb54433cd7c9e24b81ddaee447371b8
Tim Hanson <tbh391@gmail.com>
2026-01-08 18:00:56 +0100

    day 9 part 2 begun

--- day9.rkt
+++ day9.rkt
@@ -6,6 +6,9 @@
   ;part 1
   [read-positions (-> port? stream?)]
   [max-rectangle (-> port? exact-nonnegative-integer?)]
+  [green-from-to (-> pair? pair? set?)]
+  [boundary-green-tiles (-> port? set?)]
+  [max-rectangle-part-2 (-> port? exact-nonnegative-integer?)]
   ))
 
 (define (read-positions in-port)
@@ -34,6 +37,53 @@
             (* x-diff y-diff)
             max-rectangle-size)))))
           
-              
-    
+(define (green-from-to p1 p2)
+  (define (assert pred anError)
+    (if (not pred) 
+        (error anError)
+        #t))
   
+  (let ([x1 (car p1)]
+        [y1 (cdr p1)]
+        [x2 (car p2)]
+        [y2 (cdr p2)])
+      
+    (if (= x1 x2)
+        (let ([lo-y (min y1 y2)]
+              [hi-y (max y1 y2)])
+          (for/fold ([result (set)])
+                    ([y (in-range (+ lo-y 1) hi-y)])
+            (set-add result (cons x1 y))))
+        (begin
+          (assert (= y1 y2)
+                  (format "hmmm, we think either x1 = x2 or y1 = y2 must obtain! (~a ~a), (~a ~a)" x1 y1 x2 y2))
+          (let ([lo-x (min x1 x2)]
+                [hi-x (max x1 x2)])
+            (for/fold ([result (set)])
+                      ([x (in-range (+ lo-x 1) hi-x)])
+              (set-add result (cons x y1))))))))
+        
+        
+  
+
+(define (boundary-green-tiles in-port)
+  (let ([all-corners (read-positions in-port)])
+    (let ([first-last (stream-first all-corners)])
+      (define (iter green-so-far previous-corner remaining-corners)
+        (if (stream-empty? remaining-corners)
+            (set-union green-so-far (green-from-to previous-corner first-last))
+            (let ([next-corner (stream-first remaining-corners)]
+                  [new-remaining (stream-rest remaining-corners)])
+              (iter
+               (set-union green-so-far (green-from-to previous-corner next-corner))
+               next-corner
+               new-remaining))))
+
+      (iter (set) first-last (stream-rest all-corners)))))
+
+      
+      
+  
+(define (max-rectangle-part-2 in-port)
+  0)
+    
--- test-day9.rkt
+++ test-day9.rkt
@@ -3,30 +3,32 @@
 (require rackunit graph "day9.rkt")
 
 
-(let ([sample-input
-       (format
-        (string-append
-         "7,1~n"
-         "11,1~n"
-         "11,7~n"
-         "9,7~n"
-         "9,5~n"
-         "2,5~n"
-         "2,3~n"
-         "7,3~n"
-         ))])
-  (let ([in-port
-         (open-input-string sample-input)])
-    (check-equal?
-     (stream-first
-      (read-positions in-port))
-     (cons 7 1)))
+(define
+  sample-input
+  (format
+   (string-append
+    "7,1~n"
+    "11,1~n"
+    "11,7~n"
+    "9,7~n"
+    "9,5~n"
+    "2,5~n"
+    "2,3~n"
+    "7,3~n"
+    )))
 
-  (let ([in-port
-         (open-input-string sample-input)])
-    (check-equal?
-     (max-rectangle in-port)
-     50)))
+(let ([in-port
+       (open-input-string sample-input)])
+  (check-equal?
+   (stream-first
+    (read-positions in-port))
+   (cons 7 1)))
+
+(let ([in-port
+       (open-input-string sample-input)])
+  (check-equal?
+   (max-rectangle in-port)
+   50))
 
 ; part 1 full
 
@@ -39,3 +41,18 @@
 ;;    )
 ;;  )
 
+; part 2
+
+(check-exn
+   exn:fail?
+   (lambda () (green-from-to (cons 1 1) (cons 3 3))))
+
+(check-equal?
+ (green-from-to (cons 1 1) (cons 3 1))
+ (set (cons 2 1)))
+
+;; (let ([in-port
+;;        (open-input-string sample-input)])
+;;   (check-equal?
+;;    (max-rectangle-part-2 in-port)
+;;    24))
