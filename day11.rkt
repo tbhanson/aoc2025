de80b013a8a80177e681c89120fee16309595c81
Tim Hanson <tbh391@gmail.com>
2026-02-11 09:38:02 +0100

    day 11: remove deadwood

--- day11.rkt
+++ day11.rkt
@@ -9,19 +9,7 @@
   [count-paths-from-you-to-out (-> stream? exact-nonnegative-integer?)]
   [find-paths-from-node-to-out (-> stream? string? stream?)]
   ;part 2
-  [part2-path-count (-> stream? exact-nonnegative-integer?)]
-  [generator-of-paths-from-svr-to-out  (-> stream? generator?)]
-  [part2-generated-path-count (-> stream? exact-nonnegative-integer?)]
-  ; part 2, second pass
   [linked-nodes-hash (-> stream? hash?)]
-  [predecessor-nodes-hash (-> hash? hash?)]
-  ;;   
-  [find-predecessors-of-node-named (-> hash? string? set?)]
-  [find-all-predecessors-of-node-named (-> hash? string? set?)]
-
-  [generator-of-paths-from-svr-to-out-via-dac-and-fft (-> stream? generator?)]
-  [new-part2-generated-path-count (-> stream? exact-nonnegative-integer?)]
-
   ; asked claude:
   [fast-part2-count (-> stream? exact-nonnegative-integer?)]
   ))
@@ -61,29 +49,6 @@
     ;(printf "--> ~a~n" result-hash)
     result-hash))
 
-(define (find-predecessors-of-node-named linked-from-hash node-name)
-  (hash-ref linked-from-hash node-name (set)))
-
-(define (find-all-predecessors-of-node-named linked-from-hash node-name)
-  (define (iter so-far to-explore)
-    (if (set-empty? to-explore)
-        so-far
-        (let ([next-one-to-explore (set-first to-explore)]
-              [remaining-ones-to-maybe-explore (set-rest to-explore)])
-          (let ([all-new-to-explore
-                 (set-subtract
-                  (set-union
-                   (find-predecessors-of-node-named linked-from-hash next-one-to-explore)
-                   remaining-ones-to-maybe-explore)
-                  so-far)]
-                [new-so-far
-                 (set-add so-far next-one-to-explore)])
-            (iter new-so-far all-new-to-explore)))))
-
-  (set-remove
-   (iter (set) (set node-name))
-   node-name))
-    
   
 (define (count-paths-from-you-to-out graph-node-stream)
   ;(printf "(count-paths-from-you-to-out <graph-node-stream>)~n")
@@ -130,137 +95,7 @@
     (stream-map reverse (paths-to-out-from-node-named node-name (list node-name)))))
 
 
-(define (generator-of-paths-from-svr-to-out graph-node-stream)
-  ;(printf "(generator-of-paths-from-svr-to-out ...)~n")
-  (let ([node-hash (linked-nodes-hash graph-node-stream)])
-    
-    (generator ()
-               (define (paths-to-out-from-node-named node-name path-to-here)
-                 ;(printf "  (paths-to-out-from-node-named ~a ~a)~n" node-name path-to-here)
-                 (let ([linked-node-names (hash-ref node-hash node-name)])
-                   (cond [(set-member? linked-node-names "out")
-                          (yield (reverse (cons "out" path-to-here)))]
-                
-                         [else
-                          (for ([linked-node-name linked-node-names])
-                            (paths-to-out-from-node-named
-                             linked-node-name
-                             (cons linked-node-name path-to-here)))])))
-      
-               (paths-to-out-from-node-named "svr" '("svr")))))
-
-(define (part2-path-count graph-node-stream)
-  (stream-length
-   (stream-filter
-    (lambda (path)
-      (and (member "dac" path)
-           (member "fft" path)))
-    (find-paths-from-node-to-out graph-node-stream "svr"))))
-
-(define (part2-generated-path-count graph-node-stream)
-  (let ([gen (generator-of-paths-from-svr-to-out graph-node-stream)])
-    (let-values ([(sum c-dac c-fft)
-                  (for/fold ([sum 0]
-                             [saw-dac-count 0]
-                             [saw-fft-count 0])
-                            ([next-path (in-producer gen (void))] ; void as sentinel
-                             [counter (in-naturals 1)])
-                    (let ([new-sum
-                           (if (and (member "dac" next-path)
-                                    (member "fft" next-path))
-                               (+ sum 1)
-                               sum)]
-                          [new-dac-count
-                           (if (member "dac" next-path)
-                               (+ saw-dac-count 1)
-                               saw-dac-count)]
-                          [new-fft-count
-                           (if (member "fft" next-path)
-                               (+ saw-fft-count 1)
-                               saw-fft-count)])
-                                                    
-                      (cond
-                        [(= 0 (remainder counter 100000))
-                         (printf ".")]
-                        [(= 1 (remainder sum 10))
-                         (printf "s")]
-                        [(= 17 (remainder counter 500000))
-                         (printf "counter: ~a; [process time ~as] saw-dac: ~a; saw-fft: ~a; sum: ~a; next-path: ~a~n"
-                                 counter (/ (current-process-milliseconds) 1000.0) new-dac-count new-fft-count sum next-path)]
-                        )
-                      (values new-sum new-dac-count new-fft-count)
-                      ))])
-      sum)))
-
-(define (generator-of-paths-from-svr-to-out-via-dac-and-fft graph-node-stream)
-  ;(printf "(generator-of-paths-from-svr-to-out-via-dac-and-fft <graph-node-stream>)~n")
-  (let ([linked-hash (linked-nodes-hash graph-node-stream)])
-    (let ([linked-from-hash
-           (predecessor-nodes-hash linked-hash)])
-      (let ([pred-out (find-all-predecessors-of-node-named linked-from-hash "out")]
-            [pred-dac (find-all-predecessors-of-node-named linked-from-hash "dac")]
-            [pred-fft (find-all-predecessors-of-node-named linked-from-hash "fft")])
-        (let ([pred-relevant (set-intersect pred-out pred-dac pred-fft)])
- 
-    
-          (generator ()
-                     (define (paths-to-out-from-node-named node-name path-to-here)
-                       ;(printf "  (paths-to-out-from-node-named ~a ~a)~n" node-name path-to-here)
-                       (let ([linked-node-names (hash-ref linked-hash node-name)])
-                         (let ([relevant-linked-node-names
-                                (set-intersect linked-node-names pred-relevant)])
-                           (cond [(set-member? linked-node-names "out")
-                                  (yield (reverse (cons "out" path-to-here)))]
-                               
-                                 [else
-                                  (for ([linked-node-name linked-node-names])
-                                    (paths-to-out-from-node-named
-                                     linked-node-name
-                                     (cons linked-node-name path-to-here)))]))))
-      
-                     (paths-to-out-from-node-named "svr" '("svr"))))))))
-
-  ;; (define (new-part2-generated-path-count graph-node-stream)
-  ;;   (let ([gen (generator-of-paths-from-svr-to-out-via-dac-and-fft graph-node-stream)])
-  ;;     (for/fold ([sum 0])
-  ;;               ([next-path (in-producer gen (void))] ; void as sentinel
-  ;;                [counter (in-naturals 1)])
-  ;;       sum)))
-
-  (define (new-part2-generated-path-count graph-node-stream)
-    (let ([gen (generator-of-paths-from-svr-to-out-via-dac-and-fft graph-node-stream)])
-      (let-values ([(sum c-dac c-fft)
-                    (for/fold ([sum 0]
-                               [saw-dac-count 0]
-                               [saw-fft-count 0])
-                              ([next-path (in-producer gen (void))] ; void as sentinel
-                               [counter (in-naturals 1)])
-                      (let ([new-sum
-                             (if (and (member "dac" next-path)
-                                      (member "fft" next-path))
-                                 (+ sum 1)
-                                 sum)]
-                            [new-dac-count
-                             (if (member "dac" next-path)
-                                 (+ saw-dac-count 1)
-                                 saw-dac-count)]
-                            [new-fft-count
-                             (if (member "fft" next-path)
-                                 (+ saw-fft-count 1)
-                                 saw-fft-count)])
-                                                    
-                        (if (= 0 (remainder counter 100000))
-                            (printf ".")
-                            (cond [(= 1 (remainder sum 100000))
-                                   (printf "s")]
-                                
-                                  [(= 17 (remainder counter 500000))
-                                   (printf "counter: ~a; [process time ~as] saw-dac: ~a; saw-fft: ~a; sum: ~a; next-path: ~a~n"
-                                           counter (/ (current-process-milliseconds) 1000.0) new-dac-count new-fft-count sum next-path)]
-                                  ))
-                        (values new-sum new-dac-count new-fft-count)
-                        ))])
-        sum)))
+; part 2 by Claude -- nice; why didn't I think of that? :( 
 
 (define (count-paths-with-memo node-hash from-node to-node memo)
   (let ([key (cons from-node to-node)])
--- test-day11.rkt
+++ test-day11.rkt
@@ -52,49 +52,6 @@
 ;;       5))))
 
 ; part 2
-(let ([input-port
-       (open-input-string tiny-input)])
-  (let ([graph-lines (read-graph input-port)])
-    (let ([linked-hash (linked-nodes-hash graph-lines)])
-      (check-equal?
-       linked-hash
-       (hash "aaa" (set "you" "hhh") "you" (set "bbb" "ccc"))
-       )
-      (let ([linked-from-hash
-             (predecessor-nodes-hash linked-hash)])
-        (check-equal?
-         linked-from-hash
-         (hash "you" (set "aaa") "hhh" (set "aaa") "bbb" (set "you") "ccc" (set "you")))
-
-        (let ([precedes-you
-               (find-predecessors-of-node-named linked-from-hash "you")])
-          (check-equal? precedes-you (set "aaa"))
-          )))))
-      
-(let ([input-port
-       (open-input-string sample-input)])
-  (let ([graph-lines (read-graph input-port)])
-    (let ([linked-hash (linked-nodes-hash graph-lines)])
-      (let ([linked-from-hash
-             (predecessor-nodes-hash linked-hash)])
-        (let ([precedes-you
-               (find-predecessors-of-node-named linked-from-hash "you")])
-          (check-equal? precedes-you (set "aaa"))
-          
-          (check-equal?
-           (find-predecessors-of-node-named linked-from-hash "out")
-           (list->set (string-split "eee,fff,ggg,iii" ",")))
-
-          (check-equal?
-           (find-all-predecessors-of-node-named linked-from-hash "bbb")
-           (list->set (string-split "you,aaa" ",")))
-
-          (check-equal?
-           (set-count
-            (find-all-predecessors-of-node-named linked-from-hash "out"))
-           10)
-             
-          )))))
       
  
 
@@ -118,87 +75,14 @@
     )))
 
 ; 2 tests based on their part 2 sample
-(let ([part2-sample-input-port (open-input-string part2-sample-input)])
-  (let ([part2-sample-graph-lines (read-graph part2-sample-input-port)])
-    (check-true
-     (set-member?
-      (list->set
-       (stream->list
-        (find-paths-from-node-to-out part2-sample-graph-lines "svr")))
-      (string-split "svr,aaa,fft,ccc,ddd,hub,fff,ggg,out" ",")))))
 
 
-(let ([part2-sample-input-port (open-input-string part2-sample-input)])
-  (let ([part2-sample-graph-lines (read-graph part2-sample-input-port)])
-    (check-equal?
-     (part2-path-count part2-sample-graph-lines)
-     2)))
-
-;;  ; we're using sets now, so order is not deterministic; next test covers
-;; (let ([part2-sample-input-port (open-input-string part2-sample-input)])
-;;   (let ([part2-sample-graph-lines (read-graph part2-sample-input-port)])
-;;     (let ([gen (generator-of-paths-from-svr-to-out part2-sample-graph-lines)])
-;;       (check-equal?
-;;        (gen)
-;;        (string-split "svr,aaa,fft,ccc,ddd,hub,fff,ggg,out" ",")))))
-
-(let ([part2-sample-input-port (open-input-string part2-sample-input)])
-  (let ([part2-sample-graph-lines (read-graph part2-sample-input-port)])
-    (check-equal?
-     (part2-generated-path-count part2-sample-graph-lines)
-     2)))
-
-;(printf "------------ before tackling whole problem --------~n")
-
-;; a generator attempt runs seemingly forever
+; claude's answer is almost instantaneous (my previous best took forever)
 ;; (time
 ;;  (let ([input-port
 ;;         (open-input-file "test-data/input-day11-1.txt")])
 ;;    (let ([graph-lines (read-graph input-port)])
 ;;      (check-equal?
-;;       (part2-generated-path-count graph-lines)
+;;       (fast-part2-count graph-lines)
 ;;       5))))
-
-;; how many common ancestors of out, dac, and fft are there?
-(time
- (let ([input-port
-        (open-input-file "test-data/input-day11-1.txt")])
-
-   (let ([graph-lines (read-graph input-port)])
-     (let ([linked-hash (linked-nodes-hash graph-lines)])
-       (let ([linked-from-hash
-              (predecessor-nodes-hash linked-hash)])
-         (let ([pred-out (find-all-predecessors-of-node-named linked-from-hash "out")]
-               [pred-dac (find-all-predecessors-of-node-named linked-from-hash "dac")]
-               [pred-fft (find-all-predecessors-of-node-named linked-from-hash "fft")])
-                
-           (check-equal?
-            (set-count pred-out)
-            592)
-
-           (check-equal?
-            (set-count pred-dac)
-            383)
-
-           (check-equal?
-            (set-count pred-fft)
-            93)
-
-           (check-equal?
-            (set-count
-             (set-intersect pred-out pred-dac pred-fft))
-            93)
-
-           ))))))
-
-; this still takes way too long
-; .....counter: 1745500017; [process time 10805.862s] saw-dac: 95791202; saw-fft: 1745500017; sum: 95791202; next-path: (svr yco fnl sdb mzo mfh irq tev fft ikf uzy yoy anf onh jnl cwr vbf flo cnn khf rmm ocz iup yxz dwm uhx you boh vtg saz ywt igz mej out)
-(time
- (let ([input-port
-        (open-input-file "test-data/input-day11-1.txt")])
-   (let ([graph-lines (read-graph input-port)])
-     (check-equal?
-;      (new-part2-generated-path-count graph-lines)
-      (fast-part2-count graph-lines)
-      5))))
 
