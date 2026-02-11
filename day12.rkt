2a3ce9b8f785afaf984eae9f207bec07a5a53bdf
Tim Hanson <tbh391@gmail.com>
2026-02-11 09:26:09 +0100

    day 12; intent perhaps?

---
+++ day12.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(require racket/generator)
+
+(provide
+ (contract-out
+;  [read-graph (-> port? stream?)]
+  ))
+
+  
---
+++ test-day12.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(require rackunit graph "day12.rkt")
+
+
+(define
+  tiny-input
+  (format
+   (string-append
+    "aaa: you hhh~n"
+    "you: bbb ccc~n")))
+
